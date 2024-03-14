d39fcdec86c03961f405b85f174ff100c2b5f302
Krešimir <ilead7@gmail.com>
2024-03-14 10:07:10 +0100

    Konzolna napredak

---
+++ CSHARP/UcenjeWP3/UcenjeCS/Konzolna/ObradaDjelatnik.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UcenjeCS.Konzolna.Model;
+
+namespace UcenjeCS.Konzolna
+{
+    internal class ObradaDjelatnik
+    {
+
+        public List<Djelatnik> Djelatnici { get; }
+
+        public ObradaDjelatnik()
+        {
+            Djelatnici = new List<Djelatnik>();
+            if (Pomocno.dev)
+            {
+                TestniPodaci();
+            }
+
+        }
+        public void PrikaziIzbornik()
+        {
+
+            Console.WriteLine("Izbornik za rad s Djelatnicima");
+            Console.WriteLine("1. Pregled postojećih djelatnika");
+            Console.WriteLine("2. Unos novog djelatnika");
+            Console.WriteLine("3. Promjena postojećeg djelatnika");
+            Console.WriteLine("4. Brisanje djelatnika");
+            Console.WriteLine("5. Povratak na glavni izbornik");
+            switch (Pomocno.ucitajBrojRaspon("Odaberite stavku izbornika djelatnika: ",
+                "Odabir mora biti 1-5", 1, 5))
+            {
+
+                case 1:
+                    PregledDjelatnika();
+                    PrikaziIzbornik();
+                    break;
+                case 2:
+                    UcitajDjelatnika();
+                    PrikaziIzbornik();
+                    break;
+                case 3:
+                    PromjenaDjelatnika();
+                    PrikaziIzbornik();
+                    break;
+                case 4:
+                    BrisanjeDjelatnika();
+                    PrikaziIzbornik();
+                    break;
+                case 5:
+                    Console.WriteLine("Gotov rad s Djelatnicima");
+                    break;
+
+
+            }
+        }
+        private void UcitajDjelatnika()
+        {
+
+            var p = new Djelatnik();
+            p.Sifra = Pomocno.ucitajCijeliBroj("Unesite šifra polaznika: ",
+                "Unos mora biti pozitivni cijeli broj");
+            p.Ime = Pomocno.UcitajString("Unesi  ime Djelatnika: ", "Ime obavezno");
+            p.Prezime = Pomocno.UcitajString("Unesi Prezime Djelatnika: ", "Prezime obavezno");
+            Djelatnici.Add(p);
+
+        }
+        private void PromjenaDjelatnika()
+        {
+
+            PregledDjelatnika();
+            int index = Pomocno.ucitajBrojRaspon("Odaberi redni broj polaznika: ", "Nije dobar odabir", 1, Djelatnici.Count());
+            var p = Djelatnici[index - 1];
+            var originalSifra = p.Sifra;
+            var originalIme = p.Ime;
+            var originalPrezime = p.Prezime;
+            
+
+            p.Sifra = Pomocno.ucitajCijeliBroj("Unesite šifra polaznika (" + p.Sifra + "): ",
+                "Unos mora biti pozitivni cijeli broj");
+            p.Ime = Pomocno.UcitajString("Unesi ime polaznika (" + p.Ime + "): ", "Ime obavezno");
+            p.Prezime = Pomocno.UcitajString("Unesi Prezime polaznika (" + p.Prezime + "): ", "Prezime obavezno");
+            
+
+            string izbor = Pomocno.UcitajString("Želite li odustati od promjena? (da/ne): ", "Molimo unesite da ili ne");
+            if (izbor.ToLower() == "da")
+            {
+                // Poništi promjene
+                p.Sifra = originalSifra;
+                p.Ime = originalIme;
+                p.Prezime = originalPrezime;
+               
+            }
+        }
+
+        private void TestniPodaci()
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                Djelatnici.Add(new Djelatnik
+                {
+                    Sifra = i + 1,
+                    Ime = Faker.Name.First(),
+                    Prezime = Faker.Name.Last(),
+
+                });
+            }
+        }
+    }
+}
--- CSHARP/UcenjeWP3/UcenjeCS/Konzolna/Pomocno.cs
+++ CSHARP/UcenjeWP3/UcenjeCS/Konzolna/Pomocno.cs
@@ -2,60 +2,117 @@
 {
     internal class Pomocno
     {
-        public static int UcitajCijeliBroj(string poruka)
+        public static bool dev;
+
+
+        public static int ucitajBrojRaspon(string poruka, string greska,
+            int poc, int kraj)
         {
+            int b;
             while (true)
             {
-                Console.Write(poruka + ": ");
+                Console.Write(poruka);
                 try
                 {
-                    return int.Parse(Console.ReadLine());
+                    b = int.Parse(Console.ReadLine());
+                    if (b >= poc && b <= kraj)
+                    {
+                        return b;
+                    }
+                    Console.WriteLine(greska);
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
-                    Console.WriteLine("Nisi unio cijeli broj");
+                    Console.WriteLine(greska);
                 }
             }
         }
 
-        public static int UcitajCijeliBroj(string poruka, int min, int max)
+        internal static int ucitajCijeliBroj(string poruka, string greska)
         {
-            int i;
+            int b;
             while (true)
             {
-                i = UcitajCijeliBroj(poruka);
-                if (i < min || i > max)
-                {
-                    Console.WriteLine("Učitani broj nije u danom rasponu " + min + " - " + max);
-                    continue;
-                }
-                return i;
-            }
-
-
-        }
-
-        public static float UcitajDecimalniBroj(string poruka)
-        {
-            while (true)
-            {
-                Console.Write(poruka + ": ");
+                Console.Write(poruka);
                 try
                 {
-                    return float.Parse(Console.ReadLine());
+                    b = int.Parse(Console.ReadLine());
+                    if (b > 0)
+                    {
+                        return b;
+                    }
+                    Console.WriteLine(greska);
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
-                    Console.WriteLine("Nisi unio cijeli broj");
+                    Console.WriteLine(greska);
                 }
             }
         }
 
-        // Napišite metodu koja prima string poruku i vraća
-        // string koji ne smije biti prazan
-        // metoda se zove UcitajString
+        internal static decimal ucitajDecimalniBroj(string poruka, string greska)
+        {
+            decimal b;
+            while (true)
+            {
+                Console.Write(poruka);
+                try
+                {
+                    b = decimal.Parse(Console.ReadLine());
+                    if (b > 0)
+                    {
+                        return b;
+                    }
+                    Console.WriteLine(greska);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(greska);
+                }
+            }
+        }
+        internal static bool ucitajBool(string poruka)
+        {
+            Console.Write(poruka);
+            return Console.ReadLine().Trim().ToLower().Equals("da") ? true : false;
+        }
 
-        public static string UcitajString(string poruka)
+        internal static string UcitajString(string poruka, string greska)
+        {
+            string s = "";
+            while (true)
+            {
+                Console.Write(poruka);
+                s = Console.ReadLine();
+                if (s != null && s.Trim().Length > 0)
+                {
+                    return s;
+                }
+                Console.WriteLine(greska);
+            }
+        }
+
+        internal static DateTime ucitajDatum(string v1, string v2)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine(v1);
+                    return DateTime.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(v2);
+                }
+            }
+        }
+    }
+    // Napišite metodu koja prima string poruku i vraća
+    // string koji ne smije biti prazan
+    // metoda se zove UcitajString
+
+    public static string UcitajString(string poruka)
         {
             string s;
             for (; ; )
